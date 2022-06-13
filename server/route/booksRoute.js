e50adb774bb875e809b7d48c2059d0ba6843b33c
Allan Gadelha <allangadelha@gmail.com>
2022-06-13 14:27:00 -0300

    Defined structure routes; install nodemon for dev

---
+++ server/route/booksRoute.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get('/books', (req, res) => {
+    
+});
+
+router.get('/books/:id', (req, res) => {
+
+});
+
+router.post('/books', (req, res) => {
+
+});
+
+router.put('/books/:id', (req, res) => {
+
+});
+
+router.delete('/books/:id', (req, res) => {
+
+});
+
+
+module.exports = router;
