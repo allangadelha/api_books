93787c965f69cbf51b1798c0bd09648f966214bc
Allan Gadelha <allangadelha@gmail.com>
2022-06-14 16:12:27 -0300

    Creating connection with database

--- server/route/booksRoute.js
+++ server/route/booksRoute.js
@@ -1,24 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const booksService = require('../service/booksService');
 
-
-router.get('/books', (req, res) => {
-    
+router.get('/books', async function (req, res) {
+    const books = await booksService.getBooks();
+    res.json(books);
 });
 
-router.get('/books/:id', (req, res) => {
-
-});
-
-router.post('/books', (req, res) => {
+router.get('/books/:id', async function (req, res) {
 
 });
 
-router.put('/books/:id', (req, res) => {
+router.post('/books', async function (req, res) {
 
 });
 
-router.delete('/books/:id', (req, res) => {
+router.put('/books/:id', async function (req, res) {
+
+});
+
+router.delete('/books/:id', async function (req, res) {
 
 });
 
