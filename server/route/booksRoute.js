f08eee3015e937ff06fa19b767cf03389267147f
Allan Gadelha <allangadelha@gmail.com>
2022-06-15 16:10:01 -0300

    Using midlleware for handling error

--- server/route/booksRoute.js
+++ server/route/booksRoute.js
@@ -2,61 +2,61 @@ const express = require('express');
 const router = express.Router();
 const booksService = require('../service/booksService');
 
-router.get('/books', async function (req, res) {
+router.get('/books', async function (req, res,next) {
     try{
         const books = await booksService.getBooks();
         res.status(200).json(books);
     } catch (e) {
-        res.status(404).json(e.message);
+        next(e);
     }
 });
 
-router.get('/books/:id', async function (req, res) {
+router.get('/books/:id', async function (req, res,next) {
     const idBook = req.params.id;
     try {
         const book = await booksService.getBook(idBook);
         res.status(200).json(book);
     } catch (e) {
-        res.status(404).json(e.message);
+        next(e);
     }
 });
 
-router.post('/books', async function (req, res) {
+router.post('/books', async function (req, res,next) {
     const book = req.body;
     try {
         const newBook = await booksService.saveBook(book);
         res.status(201).json(newBook);
     } catch (e){
-        res.status(409).json(e.message);
+        next(e);
     }
 });
 
-router.put('/books/:id', async function (req, res) {
+router.put('/books/:id', async function (req, res,next) {
     const book = req.body;
     try {
         await booksService.updateBook(req.params.id, book);
         res.status(200).json(book);
     } catch (e) {
-        res.status(404).json(e.message);
+        next(e);
     }
 });
 
-router.patch('/books/:id', async function (req, res) {
+router.patch('/books/:id', async function (req, res,next) {
     const book = req.body;
     try {
         await booksService.updateRentedBook(req.params.id, book);
         res.status(200).json(book);
     } catch (e) {
-        res.status(404).json(e.message);
+        next(e);
     }
 });
 
-router.delete('/books/:id', async function (req, res) {
+router.delete('/books/:id', async function (req, res,next) {
     try {
         await booksService.deleteBook(req.params.id);
         res.status(204).end();
     } catch (e) {
-        res.status(404).json(e.message);
+        next(e);
     }
 });
 
--- server/server.js
+++ server/server.js
@@ -6,4 +6,14 @@ const PORT = 3000;
 app.use(express.json());
 app.use('/', require('./route/booksRoute'));
 
+app.use(function (error, req, res, next) {
+	if (error.message === 'Book already exists.') {
+		return res.status(409).send(error.message);
+	}
+	if (error.message === 'Book rented.' || error.message === 'Book not found.') {
+		return res.status(404).send(error.message);
+	}
+	res.status(500).send(error.message);
+});
+
 app.listen(PORT, () => console.log(`Aplicação iniciada na porta ${PORT}`));
